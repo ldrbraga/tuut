2814983dc1c9912da343728f8362a59da0b4d9d0
ldrbraga <ldrbraga@gmail.com>
2018-06-12 08:58:51 -0300

    banner changing

---
+++ js/banner.js
@@ -0,0 +1,6 @@
+var banners = ["../img/s2.png", "../img/s1.png"];
+function trocaBanner() {
+	document.querySelector('.carousel__img').src = banners[0];
+	banners.reverse();
+}
+setInterval(trocaBanner, 4000);
