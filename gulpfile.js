578622e09ae9cb9f3438d76159653c9a97ec9311
ldrbraga <ldrbraga@gmail.com>
2018-06-12 20:35:07 -0300

    implement gulpfile and change layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
+var cleanCss = require('gulp-clean-css');
+
+
+gulp.task('clean', function(){
+	return gulp.src('dist/')
+	.pipe(clean());
+});
+
+gulp.task('jshint', function(){
+	return gulp.src('js/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
+});
+
+gulp.task('uglify', function(){
+	return gulp.src(['assets/*.js', 'js/*.js'])
+	.pipe(uglify())
+	.pipe(concat('all.min.js'))
+	.pipe(gulp.dest('dist/js'));
+});
 
 gulp.task('htmlmin', function(){
 	return gulp.src('*.html')
@@ -7,11 +31,18 @@ gulp.task('htmlmin', function(){
 	.pipe(gulp.dest('dist/'));
 });
 
-gulp.task('copy', function(){
-	return gulp.src('css/*')
-    .pipe(gulp.dest('dist/css'))
+gulp.task('cssmin', function(){
+	return gulp.src('css/*.css')
+	.pipe(cleanCss())
+	.pipe(concat('all.min.css'))
+	.pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', ['htmlmin', 'copy']);
+gulp.task('copy', function(){
+	return gulp.src('img/*.png')
+    .pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('default', ['jshint', 'htmlmin', 'uglify', 'copy', 'cssmin']);
 
 
--- js/banner.js
+++ js/banner.js
@@ -1,4 +1,4 @@
-var banners = ["../img/s2.png", "../img/s1.png"];
+var banners = ["img/s2.png", "img/s1.png"];
 function trocaBanner() {
 	document.querySelector('.carousel__img').src = banners[0];
 	banners.reverse();
